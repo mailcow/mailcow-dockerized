977a5b4d470809edcfe8be351c88b990914ad7dc
andryyy <andre.peters@debinux.de>
2016-12-13 07:58:53 +0100

    Fix trap for postfix for proper sigterm handling

--- data/Dockerfiles/postfix/postfix.sh
+++ data/Dockerfiles/postfix/postfix.sh
@@ -1,12 +1,7 @@
 #!/bin/bash
 
-# http://superuser.com/questions/168412/using-supervisord-to-control-the-postfix-mta
+trap "postfix stop" EXIT
 
-trap "postfix stop" SIGINT
-trap "postfix stop" SIGTERM
-trap "postfix reload" SIGHUP
-
-# start postfix
 postfix -c /opt/postfix/conf start
 
 sleep infinity
