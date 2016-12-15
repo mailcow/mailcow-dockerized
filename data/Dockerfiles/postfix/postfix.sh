d6e0c46b9a6b84109e29903ce17b815dae186fce
andryyy <andre.peters@debinux.de>
2016-12-15 10:07:46 +0100

    Use entrypoint to sed some vars

--- data/Dockerfiles/postfix/postfix.sh
+++ data/Dockerfiles/postfix/postfix.sh
@@ -2,6 +2,10 @@
 
 trap "postfix stop" EXIT
 
+sed -i "/^user/c\user = ${DBUSER}" /opt/postfix/conf/sql/*
+sed -i "/^password/c\password = ${DBPASS}" /opt/postfix/conf/sql/*
+sed -i "/^dbname/c\dbname = ${DBNAME}" /opt/postfix/conf/sql/*
+
 postfix -c /opt/postfix/conf start
 
 sleep infinity
