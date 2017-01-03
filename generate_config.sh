2c822accb8f5d602529979fb1f710b56144ab74b
andryyy <andre.peters@debinux.de>
2017-01-03 10:33:06 +0100

    Do not change service names

--- generate_config.sh
+++ generate_config.sh
@@ -29,6 +29,7 @@ MAILCOW_HOSTNAME=${MAILCOW_HOSTNAME}
 # ------------------------------
 DBNAME=mailcow
 DBUSER=mailcow
+
 # Please use long, random alphanumeric strings (A-Za-z0-9)
 DBPASS=$(</dev/urandom tr -dc A-Za-z0-9 | head -c 28)
 DBROOT=$(</dev/urandom tr -dc A-Za-z0-9 | head -c 28)
@@ -36,5 +37,17 @@ DBROOT=$(</dev/urandom tr -dc A-Za-z0-9 | head -c 28)
 # ------------------------------
 # Misc configuration
 # ------------------------------
+# You should leave that alone
+# Can also be 11.22.33.44:25 or 0.0.0.0:465 etc. for specific bindings
+SMTP_PORT=25
+SMTPS_PORT=465
+SUBMISSION_PORT=587
+IMAP_PORT=143
+IMAPS_PORT=993
+POP_PORT=110
+POPS_PORT=995
+SIEVE_PORT=4190
+
+# Your timezone
 TZ=${TZ}
 EOF
