06928ec84109ecb980d811d494be003def9ec07a
andryyy <andre.peters@debinux.de>
2017-04-10 13:08:02 +0200

    imapsync: added option to delete from source after transfer

--- data/Dockerfiles/dovecot/imapsync_cron.pl
+++ data/Dockerfiles/dovecot/imapsync_cron.pl
@@ -21,7 +21,7 @@ open my $file, '<', "/etc/sogo/sieve.creds";
 my $creds = <$file>; 
 close $file;
 my ($master_user, $master_pass) = split /:/, $creds;
-my $sth = $dbh->prepare("SELECT id, user1, user2, host1, authmech1, password1, exclude, port1, enc1, delete2duplicates, maxage, subfolder2 FROM imapsync WHERE active = 1 AND (UNIX_TIMESTAMP(NOW()) - UNIX_TIMESTAMP(last_run) > mins_interval * 60 OR last_run IS NULL)");
+my $sth = $dbh->prepare("SELECT id, user1, user2, host1, authmech1, password1, exclude, port1, enc1, delete2duplicates, maxage, subfolder2, delete1 FROM imapsync WHERE active = 1 AND (UNIX_TIMESTAMP(NOW()) - UNIX_TIMESTAMP(last_run) > mins_interval * 60 OR last_run IS NULL)");
 $sth->execute();
 my $row;
 
@@ -39,6 +39,7 @@ while ($row = $sth->fetchrow_arrayref()) {
   $delete2duplicates  = @$row[9];
   $maxage             = @$row[10];
   $subfolder2         = @$row[11];
+  $delete1            = @$row[12];
 
   if ($enc1 eq "TLS") { $enc1 = "--tls1"; } elsif ($enc1 eq "SSL") { $enc1 = "--ssl1"; } else { undef $enc1; }
 
@@ -46,11 +47,12 @@ while ($row = $sth->fetchrow_arrayref()) {
 	"--timeout1", "10",
 	"--tmpdir", "/tmp",
 	"--subscribeall",
-	($exclude			eq ""	? () : ("--exclude", $exclude)),
-	($subfolder2		eq ""	? () : ('--subfolder2', $subfolder2)),
-	($maxage			eq "0"	? () : ('--maxage', $maxage)),
+	($exclude eq ""	? () : ("--exclude", $exclude)),
+	($subfolder2 eq ""	? () : ('--subfolder2', $subfolder2)),
+	($maxage eq "0"	? () : ('--maxage', $maxage)),
 	($delete2duplicates	ne "1"	? () : ('--delete2duplicates')),
-	(!defined($enc1)			? () : ($enc1)),
+	($delete1	ne "1"	? () : ('--delete')),
+	(!defined($enc1) ? () : ($enc1)),
 	"--host1", $host1,
 	"--user1", $user1,
 	"--password1", $password1,
