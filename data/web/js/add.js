f4a0289e7119b3c82e678f759eace9f4746b2f0e
andryyy <andre.peters@debinux.de>
2017-04-29 22:19:35 +0200

    Important fix: Adding a domain was not possible

--- data/web/js/add.js
+++ data/web/js/add.js
@@ -3,7 +3,7 @@ $(document).ready(function() {
 	// Get max. possible quota for a domain when domain field changes
 	$('#addSelectDomain').on('change', function() {
 		$.get("/api/v1/get/domain/" + this.value, function(data){
-      var result = jQuery.parseJSON( data );
+      var result = $.parseJSON(JSON.stringify(data));
       max_new_mailbox_quota = ( result.max_new_mailbox_quota / 1048576);
 			if (max_new_mailbox_quota != '0') {
 				$("#quotaBadge").html('max. ' +  max_new_mailbox_quota + ' MiB');
