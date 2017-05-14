e795898b80fabad951f70b25bca5a50062af1961
andryyy <andre.peters@debinux.de>
2017-05-14 20:16:43 +0200

    Fix Redis migration

--- data/Dockerfiles/php-fpm/docker-entrypoint.sh
+++ data/Dockerfiles/php-fpm/docker-entrypoint.sh
@@ -52,7 +52,7 @@ for file in $(ls /data/dkim/keys/); do
   if [[ -f /data/dkim/txt/${file} ]]; then
     redis-cli -h redis-mailcow HSET DKIM_PUB_KEYS "${domain}" "$(cat /data/dkim/txt/${file})"
     redis-cli -h redis-mailcow HSET DKIM_PRIV_KEYS "${domain}" "$(cat /data/dkim/keys/${file})"
-    redis-cli -h redis-mailcow HSET DKIM_SELECTORS "${domain}" "dkim.${domain}"
+    redis-cli -h redis-mailcow HSET DKIM_SELECTORS "${domain}" "dkim"
   fi
   rm /data/dkim/{keys,txt}/${file}
 done
