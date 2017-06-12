b367ec0ace533ee3f32bd16c70a5e7f7ab52f571
andryyy <andre.peters@debinux.de>
2017-06-12 23:48:27 +0200

    Add Unbound as better DNSSEC enabled resolver

---
+++ data/Dockerfiles/unbound/docker-entrypoint.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+unbound-control-setup
+echo "Receiving anchor key..."
+/usr/sbin/unbound-anchor -a /etc/unbound/trusted-key.key
+echo "Receiving root hints..."
+curl -#o /etc/unbound/root.hints https://www.internic.net/domain/named.cache
+
+exec "$@"
