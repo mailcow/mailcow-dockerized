d5eb85ea6b8f7cc364dc11e0b2d834b8af570f99
andryyy <andre.peters@debinux.de>
2017-06-20 00:03:09 +0200

    Added notes

--- update.sh
+++ update.sh
@@ -7,8 +7,8 @@ BRANCH=$(git rev-parse --abbrev-ref HEAD)
 
 for image in "phpfpm" "dovecot" "postfix" "sogo" "unbound" "rspamd" "clamd"; do
 if [[ ! -z $(docker images mailcow/${image} -q) ]]; then
-    echo -e "\e[32mSaving mailcow/${image} to mailcow/${image}:${DATE}...\e[90m"
-    docker tag mailcow/${image} mailcow/${image}:${DATE}
+  echo -e "\e[32mSaving mailcow/${image} to mailcow/${image}:${DATE}...\e[90m"
+  docker tag mailcow/${image} mailcow/${image}:${DATE}
 fi
 done
 
@@ -23,17 +23,17 @@ echo -e "\e[32mMerging local with remote code...\e[90m"
 git merge -Xtheirs -Xpatience -m "After update on ${DATE}"
 
 if [[ $? == 1 ]]; then
-        echo -e "\e[31mRun into conflict, trying to fix...\e[90m"
-        git status --porcelain | grep -E "UD|DU" | awk '{print $2}' | xargs rm -v
-        git add -A
-        git commit -m "After update on ${DATE}" > /dev/null
-        git checkout .
-        echo -e "\e[32mRemoved and recreated files if necessary.\e[90m"
+  echo -e "\e[31mRun into conflict, trying to fix...\e[90m"
+  git status --porcelain | grep -E "UD|DU" | awk '{print $2}' | xargs rm -v
+  git add -A
+  git commit -m "After update on ${DATE}" > /dev/null
+  git checkout .
+  echo -e "\e[32mRemoved and recreated files if necessary.\e[90m"
 fi
 echo -e "\e[32mDone!\e[0m"
 echo
 echo -e "\e[32mHashes to revert to:\e[0m"
 git reflog --color=always | grep "Before update on "
 
-# TODO: Menu, select hard reset etc.
+# TODO: Menu, select hard reset, select reset to "before update" etc.
 #git reset --hard origin/${BRANCH}
