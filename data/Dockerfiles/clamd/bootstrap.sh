ed11e7586e13834518e7d76d8537eda1b142639f
andryyy <andre.peters@debinux.de>
2017-07-04 18:05:44 +0200

    [Clamd] Add SKIP_CLAMD variable to disable Clamd start

--- data/Dockerfiles/clamd/bootstrap.sh
+++ data/Dockerfiles/clamd/bootstrap.sh
@@ -4,6 +4,11 @@ trap "kill 0" SIGINT
 touch /var/log/clamav/clamd.log /var/log/clamav/freshclam.log
 chown -R clamav:clamav /var/log/clamav/
 
+if [[ "${SKIP_CLAMD}" =~ ^([yY][eE][sS]|[yY])+$ ]]; then
+	echo "SKIP_CLAMD=y, skipping ClamAV..."
+	exit 0
+fi
+
 freshclam -d &
 clamd &
 
