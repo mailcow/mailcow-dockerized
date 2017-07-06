fa2af1b286cc128f98e2535f9020410ffabc27ff
andryyy <andre.peters@debinux.de>
2017-07-06 17:34:51 +0200

    [Web] Allow more bytes in concat queries, fixes #435

--- data/web/inc/prerequisites.inc.php
+++ data/web/inc/prerequisites.inc.php
@@ -37,7 +37,7 @@ $opt = [
   PDO::ATTR_ERRMODE            => PDO::ERRMODE_EXCEPTION,
   PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
   PDO::ATTR_EMULATE_PREPARES   => false,
-  PDO::MYSQL_ATTR_INIT_COMMAND => "SET time_zone = '" . $offset . "'",
+  PDO::MYSQL_ATTR_INIT_COMMAND => "SET time_zone = '" . $offset . "', group_concat_max_len = 3423543543;",
 ];
 try {
   $pdo = new PDO($dsn, $database_user, $database_pass, $opt);
