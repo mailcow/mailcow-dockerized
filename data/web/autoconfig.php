1b719a8668c85a59c75dc759d509673fb32c19a0
André Peters <andryyy@users.noreply.github.com>
2017-07-24 12:37:02 +0200

    Update autoconfig.php

--- data/web/autoconfig.php
+++ data/web/autoconfig.php
@@ -17,7 +17,8 @@ $domain_port = strpos($_SERVER['HTTP_HOST'], ':');
 if ($domain_port === FALSE) {
   $domain = substr($_SERVER['HTTP_HOST'], $domain_dot+1);
   $port = 443;
-} else {
+}
+else {
   $domain = substr($_SERVER['HTTP_HOST'], $domain_dot+1, $domain_port-$domain_dot-1);
   $port = substr($_SERVER['HTTP_HOST'], $domain_port+1);
 }
@@ -26,21 +27,21 @@ header('Content-Type: application/xml');
 ?>
 <?= '<?xml version="1.0"?>'; ?>
 <clientConfig version="1.1">
-    <emailProvider id="<?= $mailcow_hostname; ?>">
+    <emailProvider id="<?=$mailcow_hostname; ?>">
       <domain>%EMAILDOMAIN%</domain>
       <displayName>A mailcow mail server</displayName>
       <displayShortName>mail server</displayShortName>
 
       <incomingServer type="imap">
-         <hostname><?= $configuration['imap']['server']; ?></hostname>
-         <port><?= $configuration['imap']['port']; ?></port>
+         <hostname><?=$configuration['imap']['server']; ?></hostname>
+         <port><?=$configuration['imap']['port']; ?></port>
          <socketType>SSL</socketType>
          <username>%EMAILADDRESS%</username>
          <authentication>password-cleartext</authentication>
       </incomingServer>
       <incomingServer type="imap">
-         <hostname><?= $configuration['imap']['server']; ?></hostname>
-         <port><?= $configuration['imap']['tlsport']; ?></port>
+         <hostname><?=$configuration['imap']['server']; ?></hostname>
+         <port><?=$configuration['imap']['tlsport']; ?></port>
          <socketType>STARTTLS</socketType>
          <username>%EMAILADDRESS%</username>
          <authentication>password-cleartext</authentication>
@@ -50,8 +51,8 @@ header('Content-Type: application/xml');
 $records = dns_get_record('_pop3s._tcp.' . $domain, DNS_SRV); // check if POP3 is announced as "not provided" via SRV record
 if (count($records) == 0 || $records[0]['target'] != '') { ?>
       <incomingServer type="pop3">
-         <hostname><?= $configuration['pop3']['server']; ?></hostname>
-         <port><?= $configuration['pop3']['port']; ?></port>
+         <hostname><?=$configuration['pop3']['server']; ?></hostname>
+         <port><?=$configuration['pop3']['port']; ?></port>
          <socketType>SSL</socketType>
          <username>%EMAILADDRESS%</username>
          <authentication>password-cleartext</authentication>
@@ -61,8 +62,8 @@ if (count($records) == 0 || $records[0]['target'] != '') { ?>
 $records = dns_get_record('_pop3._tcp.' . $domain, DNS_SRV); // check if POP3 is announced as "not provided" via SRV record
 if (count($records) == 0 || $records[0]['target'] != '') { ?>
       <incomingServer type="pop3">
-         <hostname><?= $configuration['pop3']['server']; ?></hostname>
-         <port><?= $configuration['pop3']['tlsport']; ?></port>
+         <hostname><?=$configuration['pop3']['server']; ?></hostname>
+         <port><?=$configuration['pop3']['tlsport']; ?></port>
          <socketType>STARTTLS</socketType>
          <username>%EMAILADDRESS%</username>
          <authentication>password-cleartext</authentication>
@@ -70,21 +71,21 @@ if (count($records) == 0 || $records[0]['target'] != '') { ?>
 <?php } ?>
 
       <outgoingServer type="smtp">
-         <hostname><?= $configuration['smtp']['server']; ?></hostname>
-         <port><?= $configuration['smtp']['port']; ?></port>
+         <hostname><?=$configuration['smtp']['server']; ?></hostname>
+         <port><?=$configuration['smtp']['port']; ?></port>
          <socketType>SSL</socketType>
          <username>%EMAILADDRESS%</username>
          <authentication>password-cleartext</authentication>
       </outgoingServer>
       <outgoingServer type="smtp">
-         <hostname><?= $configuration['smtp']['server']; ?></hostname>
-         <port><?= $configuration['smtp']['tlsport']; ?></port>
+         <hostname><?=$configuration['smtp']['server']; ?></hostname>
+         <port><?=$configuration['smtp']['tlsport']; ?></port>
          <socketType>STARTTLS</socketType>
          <username>%EMAILADDRESS%</username>
          <authentication>password-cleartext</authentication>
       </outgoingServer>
 
-      <enable visiturl="https://<?= $mailcow_hostname; ?><?php if ($port != 443) echo ':'.$port; ?>/admin.php">
+      <enable visiturl="https://<?=$mailcow_hostname; ?><?php if ($port != 443) echo ':'.$port; ?>/admin.php">
          <instruction>If you didn't change the password given to you by the administrator or if you didn't change it in a long time, please consider doing that now.</instruction>
          <instruction lang="de">Sollten Sie das Ihnen durch den Administrator vergebene Passwort noch nicht geändert haben, empfehlen wir dies nun zu tun. Auch ein altes Passwort sollte aus Sicherheitsgründen geändert werden.</instruction>
       </enable>
@@ -92,6 +93,6 @@ if (count($records) == 0 || $records[0]['target'] != '') { ?>
     </emailProvider>
 
     <webMail>
-      <loginPage url="https://<?= $mailcow_hostname; ?><?php if ($port != 443) echo ':'.$port; ?>/SOGo/" />
+      <loginPage url="https://<?=$mailcow_hostname; ?><?php if ($port != 443) echo ':'.$port; ?>/SOGo/" />
     </webMail>
 </clientConfig>
