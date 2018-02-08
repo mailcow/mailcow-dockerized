004d262e4142d1b523ef5eb60f7f1dbaad9cc908
André Peters <andre.peters@debinux.de>
2018-02-08 22:57:17 +0100

    [Rspamd] Create missing password include as placeholder

--- data/Dockerfiles/rspamd/docker-entrypoint.sh
+++ data/Dockerfiles/rspamd/docker-entrypoint.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
 chown -R _rspamd:_rspamd /var/lib/rspamd
+[[ ! -f /etc/rspamd/override.d/worker-controller-password.inc ]] && echo '# Placeholder' > /etc/rspamd/override.d/worker-controller-password.inc
 
 exec /sbin/tini -- "$@"
