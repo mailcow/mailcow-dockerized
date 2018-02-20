3096125427a09d239b8bb344cc2f993fe87219ed
André Peters <andre.peters@debinux.de>
2018-02-20 08:34:49 +0100

    Update generate_config.sh

--- generate_config.sh
+++ generate_config.sh
@@ -4,11 +4,11 @@
 +set -o errexit
 +set -o pipefail
 
-if grep --help 2>&1 | head -n 1 | grep -q -i "busybox"; then
+if grep --help 2>&1 | grep -q -i "busybox"; then
   echo "BusybBox grep detected, please install gnu grep, \"apk add --no-cache --upgrade grep\""
   exit 1
 fi
-if cp --help 2>&1 | head -n 1 | grep -q -i "busybox"; then
+if cp --help 2>&1 | grep -q -i "busybox"; then
   echo "BusybBox cp detected, please install coreutils, \"apk add --no-cache --upgrade coreutils\""
   exit 1
 fi
