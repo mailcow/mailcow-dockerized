9d5e1599d2f1c9fafe6d30e6e83f80965fbd9f23
Philipp <admin@philipp.info>
2018-02-25 18:41:59 +0100

    Bugfixing: recipient_dependent_default_transort_maps returns now NULL in case there isn't the "relay_recipient" flag set for the domain.

--- data/Dockerfiles/postfix/postfix.sh
+++ data/Dockerfiles/postfix/postfix.sh
@@ -80,31 +80,41 @@ user = ${DBUSER}
 password = ${DBPASS}
 hosts = mysql
 dbname = ${DBNAME}
-query = SELECT GROUP_CONCAT(transport SEPARATOR '') AS transport_maps
-  FROM (
-    SELECT IF(EXISTS(SELECT 'smtp_type' FROM alias
-      LEFT OUTER JOIN mailbox ON mailbox.username = alias.goto
-        WHERE (address = '%s'
-          OR address IN (
-            SELECT CONCAT('%u', '@', target_domain) FROM alias_domain
-              WHERE alias_domain = '%d'
-          )
+query = SELECT IF(EXISTS(SELECT 'relay' FROM domain
+      WHERE active = '1'
+      AND relay_recipient = '1'
+      AND domain = '%d'
+      OR domain IN (
+        SELECT target_domain FROM alias_domain                                                                                                                                                         WHERE alias_domain = '%d'
         )
-        AND mailbox.tls_enforce_out = '1'
-        AND mailbox.active = '1'
-    ), 'smtp_enforced_tls:', 'smtp:') AS 'transport'
-    UNION ALL
-    SELECT hostname AS transport FROM relayhosts
-      LEFT OUTER JOIN domain ON domain.relayhost = relayhosts.id
-        WHERE relayhosts.active = '1'
+      ),
+    (SELECT GROUP_CONCAT(transport SEPARATOR '') AS transport_maps
+    FROM (
+      SELECT IF(EXISTS(SELECT 'smtp_type' FROM alias
+          LEFT OUTER JOIN mailbox ON mailbox.username = alias.goto
+          WHERE (address = '%s'
+            OR address IN (
+              SELECT CONCAT('%u', '@', target_domain) FROM alias_domain
+                WHERE alias_domain = '%d'
+              )
+            )
+          AND mailbox.tls_enforce_out = '1'
+          AND mailbox.active = '1'
+          ), 'smtp_enforced_tls:', 'smtp:') AS 'transport', null AS relay
+      UNION ALL
+      SELECT hostname AS transport, domain.relay_recipient AS relay FROM relayhosts
+        LEFT OUTER JOIN domain ON domain.relayhost = relayhosts.id
+          WHERE relayhosts.active = '1'
           AND relay_recipient = '1'
           AND domain = '%d'
           OR domain IN (
             SELECT target_domain FROM alias_domain
               WHERE alias_domain = '%d'
+              )
           )
-  )
-  AS transport_view;
+          AS transport_view
+    )
+    , null) AS transport_view;
 EOF
 
 cat <<EOF > /opt/postfix/conf/sql/mysql_sasl_passwd_maps.cf
