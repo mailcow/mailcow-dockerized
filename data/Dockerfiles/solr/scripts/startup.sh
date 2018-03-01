d50a325a87c426bcec6e6e0be4c8a81485ec398a
Steffen Sachse <steffen.sachse@uni-rostock.de>
2018-03-01 21:21:55 +0100

    Removed dependency from schema.xml, using api instead
    The startup.sh now has the necessary api calls to add
    the schema from scratch as a managed schema and also
    the change to the core config for solr to return results
    as xml, since this is what dovecot expects.

--- data/Dockerfiles/solr/scripts/startup.sh
+++ data/Dockerfiles/solr/scripts/startup.sh
@@ -29,13 +29,132 @@ else
     # See https://github.com/docker-solr/docker-solr/issues/27
     echo "Checking core"
     if ! wget -O - 'http://localhost:8983/solr/admin/cores?action=STATUS' | grep -q instanceDir; then
-      echo "Could not find any cores"
-      exit 1
+	echo "Could not find any cores"
+	exit 1
     fi
 
     echo "Created core with: ${@:1}"
-    echo "Copying solrconfig file"
-    cp /opt/solr/conf/schema.xml /opt/solr/server/solr/dovecot/conf/schema.xml
+    echo "Configuring schema"
+    curl -X POST -H 'Content-type:application/json' --data-binary '{
+    "add-field-type":
+    {
+	"name": "long",
+	"class": "solr.TrieLongField"
+    },
+    "add-field-type":{
+	"name": "text",
+	"class": "solr.TextField",
+	"positionIncrementGap": 100,
+	"indexAnalyser": {
+	    "tokenizer": {
+		"class": "solr.StandardTokenizerFactory",
+		"filter": {
+		    "class": "solr.WordDelimiterFilterFactory",
+		    "generateWordParts": 1,
+		    "generateNumberParts": 1,
+		    "catenateWorks": 1,
+		    "catenateNumbers": 1,
+		    "catenateAll": 0
+		}
+	    }
+	},
+	"queryAnalyzer":{
+	    "tokenizer":{
+		"class": "solr.StandardTokenizerFactory"
+	    },
+	    "filter":{
+		"synonyms": "synonyms.txt",
+		"ignoreCase": true,
+		"expand": true
+	    },
+	    "filter":{
+		"class": "solr.LowerCaseFilterFactory"
+	    }
+	}
+    },
+    "add-field":
+    {
+        "name": "uid",
+        "type": "long",
+        "indexed": true,
+        "stored": true,
+        "required": true
+    },
+    "add-field":
+    {
+        "name": "box",
+        "type": "string",
+        "indexed": true,
+        "stored": true,
+        "required": true
+    },
+    "add-field":
+    {
+        "name": "user",
+        "type": "string",
+        "indexed": true,
+        "stored": true,
+        "required": true
+    },
+    "add-field":
+    {
+        "name": "hdr",
+        "type": "text",
+        "indexed": true,
+        "stored": false,
+    },
+    "add-field":
+    {
+        "name": "body",
+        "type": "text",
+        "indexed": true,
+        "stored": false
+    },
+    "add-field":
+    {
+        "name": "from",
+        "type": "text",
+        "indexed": true,
+        "stored": false
+    },
+    "add-field":
+    {
+        "name": "to",
+        "type": "text",
+        "indexed": true,
+        "stored": false
+    },
+    "add-field":
+    {
+        "name": "cc",
+        "type": "text",
+        "indexed": true,
+        "stored": false
+    },
+    "add-field":
+    {
+        "name": "bcc",
+        "type": "text",
+        "indexed": true,
+        "stored": false
+    },
+    "add-field":
+    {
+        "name": "subject",
+        "type": "text",
+        "indexed": true,
+        "stored": false
+    }
+}' http://localhost:8983/solr/dovecot/schema
+
+    curl http://localhost:8983/solr/dovecot/config -H 'Content-type:application/json'  -d '{
+"update-requesthandler" : {
+    "name": "/select",
+    "class": "solr.SearchHandler",
+    "defaults": {"wt": "xml"}
+  }
+}'
+
     stop-local-solr
     touch $sentinel
 fi
