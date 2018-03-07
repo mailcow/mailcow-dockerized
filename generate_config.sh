83ca3c161d4568e6566dea3883cbac20fa215443
André Peters <andre.peters@debinux.de>
2018-03-07 20:28:53 +0100

    Fix generate config...

--- generate_config.sh
+++ generate_config.sh
@@ -138,10 +138,10 @@ SKIP_LETS_ENCRYPT=n
 SKIP_IP_CHECK=n
 
 # Skip ClamAV (clamd-mailcow) anti-virus (Rspamd will auto-detect a missing ClamAV container) - y/n
-SKIP_CLAMD=$(SKIP_CLAMD)
+SKIP_CLAMD=$(echo ${SKIP_CLAMD})
 
 # Skip ClamAV (clamd-mailcow) anti-virus (Rspamd will auto-detect a missing ClamAV container) - y/n
-SKIP_SOLR=$(SKIP_SOLR)
+SKIP_SOLR=$(echo ${SKIP_SOLR})
 
 # Enable watchdog (watchdog-mailcow) to restart unhealthy containers (experimental)
 USE_WATCHDOG=n
