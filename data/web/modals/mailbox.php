80009dfbef77b127f78e12373d47362fccc63bea
Samy <mail@samynitsche.de>
2018-03-12 10:31:38 +0100

    Fixed styling of input fields

--- data/web/modals/mailbox.php
+++ data/web/modals/mailbox.php
@@ -646,11 +646,15 @@ if (!isset($_SESSION['mailcow_cc_role'])) {
           </div>
 					<div class="form-group">
             <label class="control-label col-sm-2" for="ip"><?= $lang['add']['domain']; ?>/IP:</label>
-            <input type="text" class="form-control" name="ip" id="ip">
+						<div class="col-sm-10">
+            	<input type="text" class="form-control" name="ip" id="ip">
+						</div>
           </div>
 					<div class="form-group">
             <label class="control-label col-sm-2" for="port"><?= $lang['add']['port']; ?>:</label>
-            <input type="text" class="form-control" name="port" id="port">
+						<div class="col-sm-10">
+            	<input type="text" class="form-control" name="port" id="port">
+						</div>
           </div>
           <div class="form-group">
             <div class="col-sm-offset-2 col-sm-10">
