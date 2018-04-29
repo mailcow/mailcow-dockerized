97c59c2023d54653311fba1a8b724671a566c3c5
André <andre.peters@servercow.de>
2018-04-29 09:25:27 +0200

    [Update] Add fixes

--- update.sh
+++ update.sh
@@ -234,6 +234,18 @@ echo -e "Fixing project name... "
 sed -i 's#COMPOSEPROJECT_NAME#COMPOSE_PROJECT_NAME#g' mailcow.conf
 sed -i '/COMPOSE_PROJECT_NAME=/s/-//g' mailcow.conf
 
+echo -e "Fixing PHP-FPM worker ports for Nginx sites..."
+sed -i 's#9000#9002#g' data/conf/nginx/*.conf
+if ls data/conf/nginx/*.custom 1> /dev/null 2>&1; then
+  sed -i 's#9000#9002#g' data/conf/nginx/*.custom
+fi
+
+if [[ -f "data/web/nextcloud/occ" ]]; then
+echo "Setting Nextcloud Redis timeout to 0.0..."
+docker exec -it -u www-data $(docker ps -f name=php-fpm-mailcow -q) bash -c "/web/nextcloud/occ config:system:set redis timeout --value=0.0 --type=integer"
+fi
+
+
 echo -e "\e[32mStarting mailcow...\e[0m"
 sleep 2
 docker-compose up -d --remove-orphans
