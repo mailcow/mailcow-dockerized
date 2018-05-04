484d736413c50ccc289eb5fdb23f00b8bb7b24ce
Rene Kapusta <rene.kapusta@gmail.com>
2018-05-04 12:34:19 +0200

    [JSON API] fix silent json_encode() error when old INFINITE value is used at percent_in_use attribute

--- data/web/json_api.php
+++ data/web/json_api.php
@@ -150,7 +150,7 @@ if (isset($_SESSION['mailcow_cc_role']) || isset($_SESSION['pending_mailcow_cc_u
       break;
       case "get":
         function process_get_return($data) {
-          echo (!isset($data) || empty($data)) ? '{}' : json_encode($data, JSON_UNESCAPED_UNICODE | JSON_PRETTY_PRINT);
+          echo (!isset($data) || empty($data)) ? '{}' : json_encode($data, JSON_UNESCAPED_UNICODE | JSON_NUMERIC_CHECK | JSON_PRETTY_PRINT);
         }
         switch ($category) {
           case "rspamd":
