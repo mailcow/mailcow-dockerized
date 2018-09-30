cdca603ff501146692f69b29ebb907af55a191ac
André <andre.peters@servercow.de>
2018-09-30 14:43:18 +0200

    [Unbound] Fix logging, fixes #585 [Rspamd] Fix permissions of controller password file [Unbound] Enable unbound-control

--- data/Dockerfiles/rspamd/docker-entrypoint.sh
+++ data/Dockerfiles/rspamd/docker-entrypoint.sh
@@ -3,6 +3,7 @@
 chown -R _rspamd:_rspamd /var/lib/rspamd
 chmod 755 /var/lib/rspamd
 [[ ! -f /etc/rspamd/override.d/worker-controller-password.inc ]] && echo '# Placeholder' > /etc/rspamd/override.d/worker-controller-password.inc
+chown _rspamd:_rspamd /etc/rspamd/override.d/worker-controller-password.inc
 [[ ! -f /etc/rspamd/custom/sa-rules-heinlein ]] && echo '# to be auto-filled by dovecot-mailcow' > /etc/rspamd/custom/sa-rules-heinlein
 
 exec "$@"
