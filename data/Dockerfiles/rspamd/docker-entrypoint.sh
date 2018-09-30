cd01b6bb70d374b46ce76518fec2efb07fb5e5d5
André <andre.peters@servercow.de>
2018-09-30 18:55:35 +0200

    [Rspamd] Ignore custom files, but keep bad asn map

--- data/Dockerfiles/rspamd/docker-entrypoint.sh
+++ data/Dockerfiles/rspamd/docker-entrypoint.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-chown -R _rspamd:_rspamd /var/lib/rspamd
+chown -R _rspamd:_rspamd /var/lib/rspamd /etc/rspamd/local.d /etc/rspamd/override.d /etc/rspamd/custom
 chmod 755 /var/lib/rspamd
 [[ ! -f /etc/rspamd/override.d/worker-controller-password.inc ]] && echo '# Placeholder' > /etc/rspamd/override.d/worker-controller-password.inc
 chown _rspamd:_rspamd /etc/rspamd/override.d/worker-controller-password.inc
