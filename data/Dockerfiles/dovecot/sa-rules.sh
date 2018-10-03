ef341d92b91641c3567111eb539373fbc2a56a84
André <andre.peters@servercow.de>
2018-10-03 23:27:31 +0200

    [Dovecot] Add timeouts to sa-rules script, remove with -f flag to return 0

--- data/Dockerfiles/dovecot/sa-rules.sh
+++ data/Dockerfiles/dovecot/sa-rules.sh
@@ -6,7 +6,7 @@ else
   HASH_SA_RULES=$(cat /etc/rspamd/custom/sa-rules-heinlein | md5sum | cut -d' ' -f1)
 fi
 
-curl http://www.spamassassin.heinlein-support.de/$(dig txt 1.4.3.spamassassin.heinlein-support.de +short | tr -d '"').tar.gz --output /tmp/sa-rules.tar.gz
+curl --connect-timeout 15 --max-time 30 http://www.spamassassin.heinlein-support.de/$(dig txt 1.4.3.spamassassin.heinlein-support.de +short | tr -d '"').tar.gz --output /tmp/sa-rules.tar.gz
 if [[ -f /tmp/sa-rules.tar.gz ]]; then
   tar xfvz /tmp/sa-rules.tar.gz -C /tmp/sa-rules-heinlein
   # create complete list of rules in a single file
@@ -18,8 +18,8 @@ if [[ -f /tmp/sa-rules.tar.gz ]]; then
       jq -r ".[] | {name: .Config.Labels[\"com.docker.compose.service\"], id: .Id}" | \
       jq -rc "select( .name | tostring | contains(\"${CONTAINER_NAME}\")) | .id")
     if [[ ! -z ${CONTAINER_ID} ]]; then
-      curl --silent --insecure -XPOST https://dockerapi/containers/${CONTAINER_ID}/restart
+      curl --silent --insecure -XPOST --connect-timeout 15 --max-time 120 https://dockerapi/containers/${CONTAINER_ID}/restart
     fi
   fi
 fi
-rm -r /tmp/sa-rules-heinlein /tmp/sa-rules.tar.gz
+rm -rf /tmp/sa-rules-heinlein /tmp/sa-rules.tar.gz
