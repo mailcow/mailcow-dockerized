d14cef6ee2a17cafcf9a22d4760140a44f7213d9
andryyy <andre.peters@debinux.de>
2018-11-12 10:06:22 +0100

    [Config] Add info for sub.* records to generate_config.sh

--- generate_config.sh
+++ generate_config.sh
@@ -57,15 +57,18 @@ cat << EOF > mailcow.conf
 # example.org is _not_ a valid hostname, use a fqdn here.
 # Default admin user is "admin"
 # Default password is "moohoo"
+
 MAILCOW_HOSTNAME=${MAILCOW_HOSTNAME}
 
 # ------------------------------
 # SQL database configuration
 # ------------------------------
+
 DBNAME=mailcow
 DBUSER=mailcow
 
 # Please use long, random alphanumeric strings (A-Za-z0-9)
+
 DBPASS=$(LC_ALL=C </dev/urandom tr -dc A-Za-z0-9 | head -c 28)
 DBROOT=$(LC_ALL=C </dev/urandom tr -dc A-Za-z0-9 | head -c 28)
 
@@ -74,6 +77,7 @@ DBROOT=$(LC_ALL=C </dev/urandom tr -dc A-Za-z0-9 | head -c 28)
 # ------------------------------
 
 # You should use HTTPS, but in case of SSL offloaded reverse proxies:
+
 HTTP_PORT=80
 HTTP_BIND=0.0.0.0
 
@@ -99,60 +103,82 @@ DOVEADM_PORT=127.0.0.1:19991
 SQL_PORT=127.0.0.1:13306
 
 # Your timezone
+
 TZ=${MAILCOW_TZ}
 
 # Fixed project name
+
 COMPOSE_PROJECT_NAME=mailcowdockerized
 
 # Garbage collector cleanup
 # Deleted domains and mailboxes are moved to /var/vmail/_garbage/timestamp_sanitizedstring
 # How long should objects remain in the garbage until they are being deleted? (value in minutes)
 # Check interval is hourly
+
 MAILDIR_GC_TIME=1440
 
 # Additional SAN for the certificate
+#
+# You can use wildcard records to create specific names for every domain you add to mailcow.
+# Example: Add domains "example.com" and "example.net" to mailcow, change ADDITIONAL_SAN to a value like:
+#ADDITIONAL_SAN=imap.*,smtp.*
+# This will expand the certificate to "imap.example.com", "smtp.example.com", "imap.example.net", "imap.example.net"
+# plus every domain you add in the future.
+#
+# You can also just add static names...
+#ADDITIONAL_SAN=srv1.example.net
+# ...or combine wildcard and static names:
+#ADDITIONAL_SAN=imap.*,srv1.example.com
+#
+
 ADDITIONAL_SAN=
 
 # Skip running ACME (acme-mailcow, Let's Encrypt certs) - y/n
+
 SKIP_LETS_ENCRYPT=n
 
 # Skip IPv4 check in ACME container - y/n
+
 SKIP_IP_CHECK=n
 
 # Skip ClamAV (clamd-mailcow) anti-virus (Rspamd will auto-detect a missing ClamAV container) - y/n
+
 SKIP_CLAMD=n
 
 # Enable watchdog (watchdog-mailcow) to restart unhealthy containers (experimental)
+
 USE_WATCHDOG=n
+
 # Send notifications by mail (no DKIM signature, sent from watchdog@MAILCOW_HOSTNAME)
 # Can by multiple rcpts, NO quotation marks
+
 #WATCHDOG_NOTIFY_EMAIL=a@example.com,b@example.com,c@example.com
 #WATCHDOG_NOTIFY_EMAIL=
 
 # Max log lines per service to keep in Redis logs
+
 LOG_LINES=9999
 
 # Internal IPv4 /24 subnet, format n.n.n. (expands to n.n.n.0/24)
+
 IPV4_NETWORK=172.22.1
 
 # Internal IPv6 subnet in fc00::/7
+
 IPV6_NETWORK=fd4d:6169:6c63:6f77::/64
 
 # Use this IPv4 for outgoing connections (SNAT)
+
 #SNAT_TO_SOURCE=
 
 # Use this IPv6 for outgoing connections (SNAT)
-#SNAT6_TO_SOURCE=
 
-# Disable IPv6
-# mailcow-network will still be created as IPv6 enabled, all containers will be created
-# without IPv6 support.
-# Use 1 for disabled, 0 for enabled
-SYSCTL_IPV6_DISABLED=0
+#SNAT6_TO_SOURCE=
 
 # Create or override API key for web uI
 # You _must_ define API_ALLOW_FROM, which is a comma separated list of IPs
 # API_KEY allowed chars: a-z, A-Z, 0-9, -
+
 #API_KEY=
 #API_ALLOW_FROM=127.0.0.1,1.2.3.4
 
