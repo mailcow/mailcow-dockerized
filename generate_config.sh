c3d67805a1765b2c9ce04355449e117db896cce3
andryyy <andre.peters@debinux.de>
2019-01-26 19:31:01 +0100

    [Config] Add ACL_ANYONE to generate_config.sh

--- generate_config.sh
+++ generate_config.sh
@@ -148,6 +148,12 @@ TZ=${MAILCOW_TZ}
 
 COMPOSE_PROJECT_NAME=mailcowdockerized
 
+# Set this to "allow" to enable the anyone pseudo user. Disabled by default.
+# When enabled, ACL can be created, that apply to "All authenticated users"
+# This should probably only be activated on mail hosts, that are used exclusivly by one organisation.
+# Otherwise a user might share data with too many other users.
+ACL_ANYONE=disallow
+
 # Garbage collector cleanup
 # Deleted domains and mailboxes are moved to /var/vmail/_garbage/timestamp_sanitizedstring
 # How long should objects remain in the garbage until they are being deleted? (value in minutes)
