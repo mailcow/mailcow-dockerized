0c8f217f49d0a332b1da5bf49fe480bdc2af6b95
André Peters <andre.peters@debinux.de>
2019-02-23 22:20:09 +0100

    Update sogo.auth_request.template.sh
    Don't want to split hairs! Just consistency. :)

--- data/conf/nginx/templates/sogo.auth_request.template.sh
+++ data/conf/nginx/templates/sogo.auth_request.template.sh
@@ -1,6 +1,5 @@
-if printf "%s\n" "${ALLOW_ADMIN_EMAIL_LOGIN}" | grep -E '^([yY][eE][sS]|[yY])+$' >/dev/null; then
-    echo '
-auth_request /sogo-auth-verify;
+if [[ "${ALLOW_ADMIN_EMAIL_LOGIN}" =~ ^([yY][eE][sS]|[yY])+$ ]]; then
+    echo 'auth_request /sogo-auth-verify;
 auth_request_set $user $upstream_http_x_username;
 proxy_set_header x-webobjects-remote-user $user;
 '
