b7a069ee5e74a5808e94821e0067ca17bc2c8341
André Peters <andre.peters@debinux.de>
2019-02-24 00:15:09 +0100

    Update sogo-auth.php

--- data/web/sogo-auth.php
+++ data/web/sogo-auth.php
@@ -1,10 +1,9 @@
 <?php
-require_once $_SERVER['DOCUMENT_ROOT'] . '/inc/prerequisites.inc.php';
 
 /**
- * currently disabled: we could add auth_request to ningx sogo_eas.template
- * but this seems to be not required with the postfix allow_real_nets option
- */
+* currently disabled: we could add auth_request to ningx sogo_eas.template
+* but this seems to be not required with the postfix allow_real_nets option
+*/
 /*
 if (substr($_SERVER['HTTP_X_ORIGINAL_URI'], 0, 28) === "/Microsoft-Server-ActiveSync") {
   require_once $_SERVER['DOCUMENT_ROOT'] . '/inc/prerequisites.inc.php';
@@ -38,6 +37,7 @@ if (!$ALLOW_ADMIN_EMAIL_LOGIN) {
   exit;
 }
 elseif (isset($_GET['login'])) {
+  require_once $_SERVER['DOCUMENT_ROOT'] . '/inc/prerequisites.inc.php';
   if (isset($_SESSION['mailcow_cc_role']) && $_SESSION['acl']['login_as'] == "1") {
     $login = html_entity_decode(rawurldecode($_GET["login"]));
     if (filter_var($login, FILTER_VALIDATE_EMAIL)) {
@@ -53,9 +53,10 @@ elseif (isset($_GET['login'])) {
 }
 else {
   // this is an nginx auth_request call, we check for an existing sogo-sso-user session variable
+  session_start();
   $username = "";
   if (isset($_SESSION[$session_variable]) && filter_var($_SESSION[$session_variable], FILTER_VALIDATE_EMAIL)) {
-    $username = $_SESSION[$session_variable];
+      $username = $_SESSION[$session_variable];
   }
   // if username is empty, SOGo will display the normal login form
   header("X-Username: $username");
