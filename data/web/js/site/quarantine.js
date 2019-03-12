9d5758362405015b22f5a69df60af9883544af56
Kristián Feldsam <feldsam@gmail.com>
2019-03-12 21:54:31 +0100

    Quarantine - Enhanced JS + Show btn fix event binding
    Signed-off-by: Kristián Feldsam <feldsam@gmail.com>

--- data/web/js/site/quarantine.js
+++ data/web/js/site/quarantine.js
@@ -1,11 +1,13 @@
 // Base64 functions
 var Base64={_keyStr:"ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/=",encode:function(r){var t,e,o,a,h,n,c,d="",C=0;for(r=Base64._utf8_encode(r);C<r.length;)a=(t=r.charCodeAt(C++))>>2,h=(3&t)<<4|(e=r.charCodeAt(C++))>>4,n=(15&e)<<2|(o=r.charCodeAt(C++))>>6,c=63&o,isNaN(e)?n=c=64:isNaN(o)&&(c=64),d=d+this._keyStr.charAt(a)+this._keyStr.charAt(h)+this._keyStr.charAt(n)+this._keyStr.charAt(c);return d},decode:function(r){var t,e,o,a,h,n,c="",d=0;for(r=r.replace(/[^A-Za-z0-9\+\/\=]/g,"");d<r.length;)t=this._keyStr.indexOf(r.charAt(d++))<<2|(a=this._keyStr.indexOf(r.charAt(d++)))>>4,e=(15&a)<<4|(h=this._keyStr.indexOf(r.charAt(d++)))>>2,o=(3&h)<<6|(n=this._keyStr.indexOf(r.charAt(d++))),c+=String.fromCharCode(t),64!=h&&(c+=String.fromCharCode(e)),64!=n&&(c+=String.fromCharCode(o));return c=Base64._utf8_decode(c)},_utf8_encode:function(r){r=r.replace(/\r\n/g,"\n");for(var t="",e=0;e<r.length;e++){var o=r.charCodeAt(e);o<128?t+=String.fromCharCode(o):o>127&&o<2048?(t+=String.fromCharCode(o>>6|192),t+=String.fromCharCode(63&o|128)):(t+=String.fromCharCode(o>>12|224),t+=String.fromCharCode(o>>6&63|128),t+=String.fromCharCode(63&o|128))}return t},_utf8_decode:function(r){for(var t="",e=0,o=c1=c2=0;e<r.length;)(o=r.charCodeAt(e))<128?(t+=String.fromCharCode(o),e++):o>191&&o<224?(c2=r.charCodeAt(e+1),t+=String.fromCharCode((31&o)<<6|63&c2),e+=2):(c2=r.charCodeAt(e+1),c3=r.charCodeAt(e+2),t+=String.fromCharCode((15&o)<<12|(63&c2)<<6|63&c3),e+=3);return t}};
+
 jQuery(function($){
   acl_data = JSON.parse(acl);
   // http://stackoverflow.com/questions/24816/escaping-html-strings-with-jquery
   var entityMap={"&":"&amp;","<":"&lt;",">":"&gt;",'"':"&quot;","'":"&#39;","/":"&#x2F;","`":"&#x60;","=":"&#x3D;"};
   function escapeHtml(n){return String(n).replace(/[&<>"'`=\/]/g,function(n){return entityMap[n]})}
   function humanFileSize(i){if(Math.abs(i)<1024)return i+" B";var B=["KiB","MiB","GiB","TiB","PiB","EiB","ZiB","YiB"],e=-1;do{i/=1024,++e}while(Math.abs(i)>=1024&&e<B.length-1);return i.toFixed(1)+" "+B[e]}
+
   function draw_quarantine_table() {
     ft_quarantinetable = FooTable.init('#quarantinetable', {
       "columns": [
@@ -56,54 +58,52 @@ jQuery(function($){
       "empty": lang.empty,
       "paging": {"enabled": true,"limit": 5,"size": pagination_size},
       "sorting": {"enabled": true},
-      "on": {
-        "ready.ft.table": btn_group_quarantine,
-        "after.ft.paging": btn_group_quarantine
-      },
       "filtering": {"enabled": true,"position": "left","connectors": false,"placeholder": lang.filter_table},
     });
   }
 
-  btn_group_quarantine = function(ev, ft){
-    $('.show_qid_info').on('click', function (e) {
-      e.preventDefault();
-      var qitem = $(this).data('item');
-      $('#qidDetailModal').modal('show');
-      $( "#qid_error" ).hide();
-      $.ajax({
-        url: '/inc/ajax/qitem_details.php',
-        data: { id: qitem },
-        dataType: 'json',
-        success: function(data){
-          if (typeof data.error !== 'undefined') {
-            $( "#qid_error" ).text(data.error);
-            $( "#qid_error" ).show();
-          }
-          $( "li" ).each(function( index ) {
-            console.log( index + ": " + $( this ).text() );
-          });
-          $('[data-id="qitems_single"]').each(function( index ) {
-            $(this).attr("data-item", qitem);
-          });
-          $('#qid_detail_subj').text(data.subject);
-          $('#qid_detail_text').text(data.text_plain);
-          $('#qid_detail_text_from_html').text(data.text_html);
-          if (typeof data.attachments !== 'undefined') {
-            $( "#qid_detail_atts" ).text('');
-            $.each(data.attachments, function( index, value ) {
-              $( "#qid_detail_atts" ).append(
-                '<p><a href="/inc/ajax/qitem_details.php?id=' + qitem + '&att=' + index + '" target="_blank">' + value[0] + '</a> (' + value[1] + ')' +
-                ' - <small><a href="' + value[3] + '" target="_blank">' + lang.check_hash + '</a></small></p>'
-              );
-            });
-          }
-          else {
-            $( "#qid_detail_atts" ).text('-');
-          }
+  $('body').on('click', '.show_qid_info', function (e) {
+    e.preventDefault();
+    var qitem = $(this).data('item');
+    var qError = $("#qid_error");
+
+    $('#qidDetailModal').modal('show');
+    qError.hide();
+
+    $.ajax({
+      url: '/inc/ajax/qitem_details.php',
+      data: { id: qitem },
+      dataType: 'json',
+      success: function(data){
+        if (typeof data.error !== 'undefined') {
+          qError.text(data.error);
+          qError.show();
         }
-      });
-    })
-  }
+        $('[data-id="qitems_single"]').each(function(index) {
+          $(this).attr("data-item", qitem);
+        });
+
+        $('#qid_detail_subj').text(data.subject);
+        $('#qid_detail_text').text(data.text_plain);
+        $('#qid_detail_text_from_html').text(data.text_html);
+
+        if (typeof data.attachments !== 'undefined') {
+          qAtts = $("#qid_detail_atts");
+          qAtts.text('');
+          $.each(data.attachments, function(index, value) {
+            qAtts.append(
+              '<p><a href="/inc/ajax/qitem_details.php?id=' + qitem + '&att=' + index + '" target="_blank">' + value[0] + '</a> (' + value[1] + ')' +
+              ' - <small><a href="' + value[3] + '" target="_blank">' + lang.check_hash + '</a></small></p>'
+            );
+          });
+        }
+        else {
+          qAtts.text('-');
+        }
+      }
+    });
+  });
+
   // Initial table drawings
   draw_quarantine_table();
 });
