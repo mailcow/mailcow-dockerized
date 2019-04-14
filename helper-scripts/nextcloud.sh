851ee7ecbc1400fc4ad1c26eeb5fed0a5a36cc0c
andryyy <andre.peters@debinux.de>
2019-04-14 13:26:53 +0200

    [Nextcloud] Always install under subdomain, minor changes to site config and install script

--- helper-scripts/nextcloud.sh
+++ helper-scripts/nextcloud.sh
@@ -83,20 +83,13 @@ elif [[ ${NC_UPDATE} == "y" ]]; then
   fi
 
 elif [[ ${NC_INSTALL} == "y" ]]; then
-  NC_TYPE=
-  while [[ ! ${NC_TYPE} =~ ^subfolder$|^subdomain$ ]]; do
-    read -p "Configure as subdomain or subfolder? [subdomain/subfolder] " NC_TYPE
+  NC_SUBD=
+  while [[ -z ${NC_SUBD} ]]; do
+    read -p "Subdomain to run Nextcloud from [format: nextcloud.domain.tld]: " NC_SUBD
   done
-
-  if [[ ${NC_TYPE} == "subdomain" ]]; then
-    NC_SUBD=
-      while [[ -z ${NC_SUBD} ]]; do
-          read -p "Which subdomain? [format: nextcloud.domain.tld] " NC_SUBD
-      done
-    if ! ping -q -c2 ${NC_SUBD} > /dev/null 2>&1 ; then
-      read -p "Cannot ping subdomain, continue anyway? [y|N] " NC_CONT_FAIL
-      [[ ! ${NC_CONT_FAIL,,} =~ ^(yes|y)$ ]] && { echo "Ok, exiting..."; exit 1; }
-    fi
+  if ! ping -q -c2 ${NC_SUBD} > /dev/null 2>&1 ; then
+    read -p "Cannot ping subdomain, continue anyway? [y|N] " NC_CONT_FAIL
+    [[ ! ${NC_CONT_FAIL,,} =~ ^(yes|y)$ ]] && { echo "Ok, exiting..."; exit 1; }
   fi
 
   ADMIN_NC_PASS=$(</dev/urandom tr -dc A-Za-z0-9 | head -c 28)
@@ -107,7 +100,7 @@ elif [[ ${NC_INSTALL} == "y" ]]; then
     && mkdir -p ./data/web/nextcloud/data \
     && chmod +x ./data/web/nextcloud/occ
 
-  docker exec -it $(docker ps -f name=php-fpm-mailcow -q) /bin/bash -c "chown -R www-data:www-data /web/nextcloud/data /web/nextcloud/config /web/nextcloud/apps"
+  docker exec -it $(docker ps -f name=php-fpm-mailcow -q) /bin/bash -c "chown -R www-data:www-data /web/nextcloud"
   docker exec -it -u www-data $(docker ps -f name=php-fpm-mailcow -q) /web/nextcloud/occ --no-warnings maintenance:install \
     --database mysql \
     --database-host mysql \
@@ -136,22 +129,21 @@ elif [[ ${NC_INSTALL} == "y" ]]; then
     /web/nextcloud/occ --no-warnings config:system:set mail_from_address --value=nextcloud; \
     /web/nextcloud/occ --no-warnings config:system:set mail_domain --value=${MAILCOW_HOSTNAME}; \
     /web/nextcloud/occ --no-warnings config:system:set mail_smtphost --value=postfix; \
-    /web/nextcloud/occ --no-warnings config:system:set mail_smtpport --value=588
-    /web/nextcloud/occ --no-warnings app:install user_external
-    /web/nextcloud/occ --no-warnings config:system:set user_backends 0 arguments 0 --value={dovecot:143/imap/tls/novalidate-cert}
-    /web/nextcloud/occ --no-warnings config:system:set user_backends 0 class --value=OC_User_IMAP
+    /web/nextcloud/occ --no-warnings config:system:set mail_smtpport --value=588; \
+    /web/nextcloud/occ --no-warnings config:system:set trusted_domains 1 --value=${NC_SUBD}; \
+    /web/nextcloud/occ --no-warnings config:system:set overwritewebroot --value=/; \
+    /web/nextcloud/occ --no-warnings config:system:set overwritehost --value=${NC_SUBD}; \
     /web/nextcloud/occ --no-warnings db:convert-filecache-bigint -n"
 
-  if [[ ${NC_TYPE} == "subdomain" ]]; then
-    docker exec -it -u www-data $(docker ps -f name=php-fpm-mailcow -q) /web/nextcloud/occ --no-warnings config:system:set trusted_domains 1 --value=${NC_SUBD}
-    docker exec -it -u www-data $(docker ps -f name=php-fpm-mailcow -q) /web/nextcloud/occ --no-warnings config:system:set overwritewebroot --value=/
-    docker exec -it -u www-data $(docker ps -f name=php-fpm-mailcow -q) /web/nextcloud/occ --no-warnings config:system:set overwritehost --value=${NC_SUBD}
+    # Not installing by default, broke too often
+    #/web/nextcloud/occ --no-warnings app:install user_external; \
+    #/web/nextcloud/occ --no-warnings config:system:set user_backends 0 arguments 0 --value={dovecot:143/imap/tls/novalidate-cert}; \
+    #/web/nextcloud/occ --no-warnings config:system:set user_backends 0 class --value=OC_User_IMAP; \
+
     cp ./data/assets/nextcloud/nextcloud.conf ./data/conf/nginx/
     sed -i "s/NC_SUBD/${NC_SUBD}/g" ./data/conf/nginx/nextcloud.conf
-  elif [[ ${NC_TYPE} == "subfolder" ]]; then
-    cp ./data/assets/nextcloud/site.nextcloud.custom ./data/conf/nginx/
-  fi
 
+  echo "Restarting Nginx..."
   docker restart $(docker ps -aqf name=nginx-mailcow)
 
   echo "Login as admin with password: ${ADMIN_NC_PASS}"
