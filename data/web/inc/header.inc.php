85a33208ffc43d61c0e458efbfb57573f1170600
andryyy <andre.peters@debinux.de>
2019-07-22 21:11:25 +0200

    [Web] Disable user-scalable, set max-scale to 1

--- data/web/inc/header.inc.php
+++ data/web/inc/header.inc.php
@@ -3,7 +3,7 @@
 <head>
   <meta charset="utf-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
-  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=0">
   <meta name="theme-color" content="#F5D76E"/>
   <meta http-equiv="Referrer-Policy" content="same-origin">
   <title><?=$UI_TEXTS['title_name'];?></title>
