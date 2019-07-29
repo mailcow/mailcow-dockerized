7cbe710d914fabdf831631d91cfb9d59d92b80c8
andryyy <andre.peters@debinux.de>
2019-07-29 16:16:31 +0200

    [Web] unlimited_quota fix for domain admins

--- data/web/inc/functions.mailbox.inc.php
+++ data/web/inc/functions.mailbox.inc.php
@@ -775,7 +775,7 @@ function mailbox($_action, $_type, $_data = null, $_extra = null) {
           $password2    = $_data['password2'];
           $name         = ltrim(rtrim($_data['name'], '>'), '<');
           $quota_m			= intval($_data['quota']);
-          if ((!isset($_SESSION['acl']['unlimited_quota']) || $_SESSION['acl']['quarantine_notification'] != "1") && $quota_m === 0) {
+          if ((!isset($_SESSION['acl']['unlimited_quota']) || $_SESSION['acl']['unlimited_quota'] != "1") && $quota_m === 0) {
             $_SESSION['return'][] = array(
               'type' => 'danger',
               'log' => array(__FUNCTION__, $_action, $_type, $_data_log, $_attr),
