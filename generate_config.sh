af8ed1d33a64cdbe73a20c13854a70d5e87681ed
andryyy <andre.peters@debinux.de>
2019-12-05 19:18:06 +0100

    [Install] Ubuntu 16.04 with default kernel is also unsupported in gen_conf

--- generate_config.sh
+++ generate_config.sh
@@ -8,6 +8,14 @@ if [[ "$(uname -r)" =~ ^4\.15\.0-60 ]]; then
   exit 1
 fi
 
+if [[ "$(uname -r)" =~ ^4\.4\. ]]; then
+  if grep -q Ubuntu <<< $(uname -a); then
+    echo "DO NOT RUN mailcow ON THIS UBUNTU KERNEL!";
+    echo "Please update to linux-generic-hwe-16.04 by running \"apt-get install --install-recommends linux-generic-hwe-16.04\""
+  fi
+  exit 1
+fi
+
 if grep --help 2>&1 | grep -q -i "busybox"; then
   echo "BusybBox grep detected, please install gnu grep, \"apk add --no-cache --upgrade grep\""
   exit 1
