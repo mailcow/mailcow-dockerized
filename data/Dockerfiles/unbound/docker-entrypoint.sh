fb30e8c076dcd482bde674dea4948af597277e16
Kevin Klopfenstein <kk@sudo-i.net>
2019-12-08 22:09:44 +0100

    Update docker-entrypoint.sh
    there is a typo this way unbound wont work properly because the root hints cant be downloaded

--- data/Dockerfiles/unbound/docker-entrypoint.sh
+++ data/Dockerfiles/unbound/docker-entrypoint.sh
@@ -6,7 +6,7 @@ chmod g+rw /dev/console
 echo "Receiving anchor key..."
 /usr/sbin/unbound-anchor -a /etc/unbound/trusted-key.key
 echo "Receiving root hints..."
-curl -#o /etc/unbound/root.hints https://www.internic.net/domain/named.cache
+curl -o /etc/unbound/root.hints https://www.internic.net/domain/named.cache
 /usr/sbin/unbound-control-setup
 
 # Run hooks
