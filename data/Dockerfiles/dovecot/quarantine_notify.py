2f818b7f846ec6d34146ff59ac183d049b655498
andryyy <andre.peters@debinux.de>
2020-01-10 20:43:20 +0100

    [Dovecot] Set bcc in quarantine notify

--- data/Dockerfiles/dovecot/quarantine_notify.py
+++ data/Dockerfiles/dovecot/quarantine_notify.py
@@ -84,8 +84,9 @@ def notify_rcpt(rcpt, msg_count, quarantine_acl):
       msg.attach(text_part)
       msg.attach(html_part)
       msg['To'] = str(rcpt)
+      bcc = r.get('Q_GLOBAL_RCPT') or ""
       text = msg.as_string()
-      server.sendmail(msg['From'], msg['To'], text)
+      server.sendmail(msg['From'], [str(rcpt)] + [str(bcc)], text)
       server.quit()
       for res in meta_query:
         query_mysql('UPDATE quarantine SET notified = 1 WHERE id = "%d"' % (res['id']), update = True)
