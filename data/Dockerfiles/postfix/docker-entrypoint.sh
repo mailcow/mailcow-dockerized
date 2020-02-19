c54cfbf332fbd01fb0170282d12f1c2f7590d3e6
andryyy <andre.peters@debinux.de>
2020-02-19 21:41:06 +0100

    [Postfix] Add hooks

--- data/Dockerfiles/postfix/docker-entrypoint.sh
+++ data/Dockerfiles/postfix/docker-entrypoint.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
 
+# Run hooks
+for file in /hooks/*; do
+  if [ -x "${file}" ]; then
+    echo "Running hook ${file}"
+    "${file}"
+  fi
+done
+
 if [[ ! -z ${REDIS_SLAVEOF_IP} ]]; then
   cp /etc/syslog-ng/syslog-ng-redis_slave.conf /etc/syslog-ng/syslog-ng.conf
 fi
