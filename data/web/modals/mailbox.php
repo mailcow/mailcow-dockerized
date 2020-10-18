2d312603ea6c1fdeb0d4f6e8e6e85bd2a6aec11d
Kristian Feldsam <feldsam@gmail.com>
2020-10-18 21:10:27 +0200

    [Web] Mailbox create/edit check if unlimited quota available
    Signed-off-by: Kristian Feldsam <feldsam@gmail.com>
    
    # Conflicts:
    #	data/web/modals/mailbox.php

--- data/web/modals/mailbox.php
+++ data/web/modals/mailbox.php
@@ -42,9 +42,13 @@ if (!isset($_SESSION['mailcow_cc_role'])) {
             <label class="control-label col-sm-2" for="addInputQuota"><?=$lang['add']['quota_mb'];?>
               <br /><span id="quotaBadge" class="badge">max. - MiB</span>
             </label>
-            <div class="col-sm-10">
-            <input type="text" class="form-control" name="quota" min="0" max="" id="addInputQuota" disabled value="<?=$lang['add']['select_domain'];?>" required>
+            <?php
+              $quotaMin = (!isset($_SESSION['acl']['unlimited_quota']) || $_SESSION['acl']['unlimited_quota'] != "1") > 0) ? 1 : 0;
+            ?>
+            <input type="text" class="form-control" name="quota" min="<?=$quotaMin?>" max="" id="addInputQuota" disabled value="<?=$lang['add']['select_domain'];?>" required>
+            <?php if($quotaMin === 0): ?>
             <small class="help-block">0 = ∞</small>
+            <?php endif; ?>
             <div class="label label-warning addInputQuotaExhausted" style="display:none;"><?=$lang['warning']['quota_exceeded_scope'];?></div>
             </div>
           </div>
