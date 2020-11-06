4e337b308f56345138a6c0ebf3b7bb1f9f9c046e
andryyy <andre.peters@debinux.de>
2020-11-06 12:23:02 +0100

    [Dovecot] Select action in quarantine script, use nobody to run imapsync cron

--- data/Dockerfiles/dovecot/docker-entrypoint.sh
+++ data/Dockerfiles/dovecot/docker-entrypoint.sh
@@ -321,7 +321,7 @@ chmod +x /usr/lib/dovecot/sieve/rspamd-pipe-ham \
 
 if [[ "${MASTER}" =~ ^([yY][eE][sS]|[yY])+$ ]]; then
 # Setup cronjobs
-echo '* * * * *    root  /usr/local/bin/imapsync_cron.pl 2>&1 | /usr/bin/logger' > /etc/cron.d/imapsync
+echo '* * * * *    nobody  /usr/local/bin/imapsync_cron.pl 2>&1 | /usr/bin/logger' > /etc/cron.d/imapsync
 #echo '30 3 * * *   vmail /usr/local/bin/doveadm quota recalc -A' > /etc/cron.d/dovecot-sync
 echo '* * * * *    vmail /usr/local/bin/trim_logs.sh >> /dev/console 2>&1' > /etc/cron.d/trim_logs
 echo '25 * * * *   vmail /usr/local/bin/maildir_gc.sh >> /dev/console 2>&1' > /etc/cron.d/maildir_gc
