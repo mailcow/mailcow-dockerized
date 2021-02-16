11a858a7fd90c253b132447735517a3fba8f531a
andryyy <andre.peters@debinux.de>
2021-02-16 21:33:46 +0100

    [Web] Add Trustkey CA for WebAuthn

--- data/web/inc/prerequisites.inc.php
+++ data/web/inc/prerequisites.inc.php
@@ -67,6 +67,7 @@ $WebAuthn->addRootCertificates($_SERVER['DOCUMENT_ROOT'] . '/inc/lib/WebAuthn/ro
 $WebAuthn->addRootCertificates($_SERVER['DOCUMENT_ROOT'] . '/inc/lib/WebAuthn/rootCertificates/googleHardware.pem');
 $WebAuthn->addRootCertificates($_SERVER['DOCUMENT_ROOT'] . '/inc/lib/WebAuthn/rootCertificates/microsoftTpmCollection.pem');
 $WebAuthn->addRootCertificates($_SERVER['DOCUMENT_ROOT'] . '/inc/lib/WebAuthn/rootCertificates/huawei.pem');
+$WebAuthn->addRootCertificates($_SERVER['DOCUMENT_ROOT'] . '/inc/lib/WebAuthn/rootCertificates/trustkey.pem');
 
 // Redis
 $redis = new Redis();
