5be3dab669f9aba447a880912ac7df13670af39d
Martin Winter <dev@macwinnie.me>
2021-06-23 22:28:33 +0200

    remove change of alias map in order of discussion with @andryyy

--- data/Dockerfiles/postfix/postfix.sh
+++ data/Dockerfiles/postfix/postfix.sh
@@ -223,17 +223,9 @@ user = ${DBUSER}
 password = ${DBPASS}
 hosts = unix:/var/run/mysqld/mysqld.sock
 dbname = ${DBNAME}
-query = SELECT COALESCE (
-    (
-      SELECT goto FROM alias
-        WHERE address='%s'
-        AND (active='1' OR active='2')
-    ), (
-      SELECT goto FROM alias
-        WHERE address='@%d'
-        AND (active='1' OR active='2')
-    )
-  );
+query = SELECT goto FROM alias
+  WHERE address='%s'
+    AND (active='1' OR active='2');
 EOF
 
 cat <<EOF > /opt/postfix/conf/sql/mysql_recipient_bcc_maps.cf
