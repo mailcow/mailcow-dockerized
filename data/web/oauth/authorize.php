72ceeda8727010aed926ea72cae9b56388ab639e
Kristian Feldsam <feldsam@gmail.com>
2021-10-22 14:37:26 +0200

    [Web] oauth authorize - added missing twig templating

--- data/web/oauth/authorize.php
+++ data/web/oauth/authorize.php
@@ -11,49 +11,18 @@ $response = new OAuth2\Response();
 
 if (!$oauth2_server->validateAuthorizeRequest($request, $response)) {
   $response->send();
-  exit();
+  exit;
 }
 
-if (!isset($_POST['authorized'])):
-require_once $_SERVER['DOCUMENT_ROOT'] . '/inc/header.inc.php';
+if (!isset($_POST['authorized'])) {
+  require_once $_SERVER['DOCUMENT_ROOT'] . '/inc/header.inc.php';
 
-?>
-<div class="container">
-  <div class="panel panel-default">
-    <div class="panel-heading"><?=$lang['oauth2']['authorize_app'];?></div>
-    <div class="panel-body">
-      <?php
-      if ($_SESSION['mailcow_cc_role'] != 'user'):
-      $request = '';
-      ?>
-      <p><?=$lang['oauth2']['access_denied'];?></p>
-      <?php
-      else:
-      ?>
-      <p><?=$lang['oauth2']['scope_ask_permission'];?>:</p>
-      <dl class="dl-horizontal">
-        <dt><?=$lang['oauth2']['profile'];?></dt>
-        <dd><?=$lang['oauth2']['profile_desc'];?></dd>
-      </dl>
-      <form class="form-horizontal" autocapitalize="none" autocorrect="off" role="form" method="post">
-        <div class="form-group">
-          <div class="col-sm-10 text-center">
-            <button class="btn btn-success" name="authorized" type="submit" value="1"><?=$lang['oauth2']['permit'];?></button>
-            <a href="#" class="btn btn-default" onclick="window.history.back()" role="button"><?=$lang['oauth2']['deny'];?></a>
-            <input type="hidden" name="csrf_token" value="<?=$_SESSION['CSRF']['TOKEN'];?>">
-          </div>
-        </div>
-      </form>
-      <?php
-      endif;
-      ?>
-    </div>
-  </div>
-</div> <!-- /container -->
-<?php
-require_once $_SERVER['DOCUMENT_ROOT'] . '/inc/footer.inc.php';
-exit();
-endif;
+  $template = 'oauth/authorize.twig';
+  $template_data = [];
+
+  require_once $_SERVER['DOCUMENT_ROOT'] . '/inc/footer.inc.php';
+  exit;
+}
 
 // print the authorization code if the user has authorized your client
 $is_authorized = ($_POST['authorized'] == '1');
