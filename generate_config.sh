8ded05c20f914abc3daa674efddf282d0cd61bfb
Ranoiaetep <55516229+Ranoiaetep@users.noreply.github.com>
2021-10-23 11:49:04 -0400

    [Update] Fix exit in generate_config.sh (#4274)
    Line 16 made an exit without any output

--- generate_config.sh
+++ generate_config.sh
@@ -12,8 +12,8 @@ if [[ "$(uname -r)" =~ ^4\.4\. ]]; then
   if grep -q Ubuntu <<< $(uname -a); then
     echo "DO NOT RUN mailcow ON THIS UBUNTU KERNEL!";
     echo "Please update to linux-generic-hwe-16.04 by running \"apt-get install --install-recommends linux-generic-hwe-16.04\""
+    exit 1
   fi
-  exit 1
 fi
 
 if grep --help 2>&1 | grep -q -i "busybox"; then
