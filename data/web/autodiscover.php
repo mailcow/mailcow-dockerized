59ee906b53acb25ff374451f415b476bc42b9588
Jürgen Kellerer <juergen@k123.eu>
2021-10-24 13:12:34 +0200

    Supporting app-passwds in cal/carddav & ActiveSync

--- data/web/autodiscover.php
+++ data/web/autodiscover.php
@@ -67,7 +67,8 @@ if (empty($_SERVER['PHP_AUTH_USER']) || empty($_SERVER['PHP_AUTH_PW'])) {
   exit(0);
 }
 
-$login_role = check_login($login_user, $login_pass);
+$allow_app_passwords = $ALLOW_APP_PASSWORDS_IN_EAS === true || $autodiscover_config['autodiscoverType'] == 'imap';
+$login_role = check_login($login_user, $login_pass, $allow_app_passwords);
 
 if ($login_role === "user") {
   header("Content-Type: application/xml");
