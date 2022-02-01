89fdd1986d922aa7ae87d589859cd3d483beaa23
Niklas Meyer <62480600+DerLinkman@users.noreply.github.com>
2022-02-01 15:26:48 +0100

    Jan(moo)uary Update 2022 - Revision A (2022-01a) (#4445)
    * [API] Fix minor issue in api docs
    
    * [GH-Actions][stale] Add neverstale label to exempt list
    
    * [Web] add github version tag
    
    * [Web] add github version tag error handling
    
    * Passwordless SOGo auth: support for calendar invitations and calendar/contacts subscriptions
    
    Inviting someone to a calendar event triggers a request to /SOGo/so/otheruser@example.com/freebusy.ifb/ajaxRead. Subscribing to someone's calendar/contacts triggers a request to /SOGo/so/otheruser@example.com/foldersSearch. The email address in the URL is different from the logged-in user, which needs to be handled appropriately by sogo-auth.php.
    
    * [Web] add github version tag - adjust css
    
    * [Compose] Update SOGo Autoreply Schedule to 5m
    
    Based on the advice of inverse (SOGo developer). Thanks to https://github.com/jmber
    
    Closes: https://github.com/mailcow/mailcow-dockerized/issues/4436
    
    * [Web] add github version tag - move twig globals
    
    * [Web] add github version tag - missing </div>
    
    * Passwordless SOGo auth: improvements for when accessing other users
    
    * [WebAuthn] fido2 passwordless auth - fix (#4440)
    
    * [WebAuthn] fido2 revert
    
    * [WebAuthn] set UV flags to 'discouraged'
    
    * [WebAuthn] revert - set UV flags to 'discouraged'
    
    Co-authored-by: ntimo <git@nowitzki.me>
    Co-authored-by: Peter <magic@kthx.at>
    Co-authored-by: FreddleSpl0it <patschul@posteo.de>
    Co-authored-by: FreddleSpl0it <75116288+FreddleSpl0it@users.noreply.github.com>
    Co-authored-by: Michael Kuron <mkuron@users.noreply.github.com>

--- update.sh
+++ update.sh
@@ -719,6 +719,22 @@ if [ -f "data/conf/rspamd/local.d/metrics.conf" ]; then
   mv data/conf/rspamd/local.d/metrics.conf data/conf/rspamd/local.d/metrics.conf_deprecated
 fi
 
+# Set app_info.inc.php
+mailcow_git_version=$(git describe --tags `git rev-list --tags --max-count=1`)
+if [ $? -eq 0 ]; then
+  mailcow_git_url=$(git config --get remote.origin.url)
+  echo '<?php' > data/web/inc/app_info.inc.php
+  echo '  $MAILCOW_GIT_VERSION="'$mailcow_git_version'";' >> data/web/inc/app_info.inc.php
+  echo '  $MAILCOW_GIT_URL="'$mailcow_git_url'";' >> data/web/inc/app_info.inc.php
+  echo '?>' >> data/web/inc/app_info.inc.php
+else
+  echo '<?php' > data/web/inc/app_info.inc.php
+  echo '  $MAILCOW_GIT_VERSION="";' >> data/web/inc/app_info.inc.php
+  echo '  $MAILCOW_GIT_URL="";' >> data/web/inc/app_info.inc.php
+  echo '?>' >> data/web/inc/app_info.inc.php
+  echo -e "\e[33mCannot determine current git repository version...\e[0m"
+fi
+
 if [[ ${SKIP_START} == "y" ]]; then
   echo -e "\e[33mNot starting mailcow, please run \"docker-compose up -d --remove-orphans\" to start mailcow.\e[0m"
 else
