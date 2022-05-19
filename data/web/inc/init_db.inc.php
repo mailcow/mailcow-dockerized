97df5c3b9c3b856aa08a88ea7c6a961dc749ace2
DerLinkman <derlinkman@gmail.com>
2022-05-19 15:42:13 +0200

    [DB] Update DB Version to remove pipemess parameters

--- data/web/inc/init_db.inc.php
+++ data/web/inc/init_db.inc.php
@@ -3,7 +3,7 @@ function init_db_schema() {
   try {
     global $pdo;
 
-    $db_version = "02052022_1500";
+    $db_version = "19052022_1541";
 
     $stmt = $pdo->query("SHOW TABLES LIKE 'versions'");
     $num_results = count($stmt->fetchAll(PDO::FETCH_ASSOC));
