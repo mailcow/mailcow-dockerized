37b4ff811d55f0e718ec16d6919a02be7d60262e
FreddleSpl0it <patschul@posteo.de>
2022-06-14 16:31:21 +0200

    [BS5] add theme selector

--- data/web/inc/prerequisites.inc.php
+++ data/web/inc/prerequisites.inc.php
@@ -252,7 +252,7 @@ $css_minifier = new CSSminifierExtended();
 $css_dir = array_diff(scandir('/web/css/build'), array('..', '.'));
 // get customized ui data
 $UI_TEXTS = customize('get', 'ui_texts');
-$UI_THEME = "lumen"; // TODO: customize('get', 'ui_theme');
+$UI_THEME = customize('get', 'ui_theme');
 // minify bootstrap theme
 if (file_exists('/web/css/themes/'.$UI_THEME.'-bootstrap.css'))
   $css_minifier->add('/web/css/themes/'.$UI_THEME.'-bootstrap.css');
