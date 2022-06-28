b0fe22c04f7ffdd86a578b10db99c88dd3494601
FreddleSpl0it <patschul@posteo.de>
2022-06-28 07:20:46 +0200

    [BS5] add darkmode

--- data/web/js/build/014-mailcow.js
+++ data/web/js/build/014-mailcow.js
@@ -322,15 +322,10 @@ $(document).ready(function() {
   }
 
   // Dark Mode Loader
-  // check if darkmode is preferred by OS
-  if (window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches) 
-    toggleDarkMode();
-  // check if darkmode is set by localStorage
-  if (JSON.parse(localStorage.getItem("darkmode")) === true)
-    toggleDarkMode();
-  // register dark mode toggle event listener
+  if ($('#dark-mode-theme').length)
+    $('#dark-mode-toggle').prop('checked', true);
+
   $('#dark-mode-toggle').click(toggleDarkMode);
-  // dark mode toggle funtion
   function toggleDarkMode(){
     if($('#dark-mode-theme').length){
       $('#dark-mode-theme').remove();
