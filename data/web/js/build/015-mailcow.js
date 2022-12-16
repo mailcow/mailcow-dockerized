f1bb23ba2a9b15f5c61e91b2d299eda130a3eae8
FreddleSpl0it <patschul@posteo.de>
2022-12-16 09:40:20 +0100

    fix darkmode toggle

--- data/web/js/build/015-mailcow.js
+++ data/web/js/build/015-mailcow.js
@@ -304,13 +304,13 @@ $(document).ready(function() {
       $('#dark-mode-toggle').prop('checked', false);
       if ($('#rspamd_logo').length) $('#rspamd_logo').attr('src', '/img/rspamd_logo_dark.png');
       if ($('#rspamd_logo_sm').length) $('#rspamd_logo_sm').attr('src', '/img/rspamd_logo_dark.png');
-      localStorage.setItem('darkmode', 'false');
+      localStorage.setItem('theme', 'light');
     }else{
       $('head').append('<link id="dark-mode-theme" rel="stylesheet" type="text/css" href="/css/themes/mailcow-darkmode.css">');
       $('#dark-mode-toggle').prop('checked', true);
       if ($('#rspamd_logo').length) $('#rspamd_logo').attr('src', '/img/rspamd_logo_light.png');
       if ($('#rspamd_logo_sm').length) $('#rspamd_logo_sm').attr('src', '/img/rspamd_logo_light.png');
-      localStorage.setItem('darkmode', 'true');
+      localStorage.setItem('theme', 'dark');
     }
   }
 });
