0cad07704258788e4e509d3a3081fcc70d651633
FreddleSpl0it <patschul@posteo.de>
2024-03-01 00:47:48 +0100

    [Nginx] Use Jinja2 for configuration templates

---
+++ data/Dockerfiles/nginx/bootstrap-nginx.py
@@ -0,0 +1,108 @@
+import subprocess
+import os
+from jinja2 import Environment, FileSystemLoader
+import time
+
+
+def sites_default_conf(env):
+    config_name = "sites-default.conf"
+    template = env.get_template(f"{config_name}.j2")
+
+    IPV4_NETWORK = os.getenv("IPV4_NETWORK", "172.22.1")
+    sogo_proxy_auth = """
+    auth_request /sogo-auth-verify;
+    auth_request_set $user $upstream_http_x_user;
+    auth_request_set $auth $upstream_http_x_auth;
+    auth_request_set $auth_type $upstream_http_x_auth_type;
+    proxy_set_header x-webobjects-remote-user "$user";
+    proxy_set_header Authorization "$auth";
+    proxy_set_header x-webobjects-auth-type "$auth_type";
+    """
+    sogo_proxy_pass = f"proxy_pass http://{IPV4_NETWORK}.248:20000;"
+    if os.getenv("SKIP_SOGO", "n").lower() in ("y", "yes"):
+        sogo_eas_proxy_pass = "return 410;"
+    else:
+        sogo_eas_proxy_pass = f"proxy_pass http://{IPV4_NETWORK}.248:20000/SOGo/Microsoft-Server-ActiveSync;"
+
+
+    config = template.render(
+        sogo_proxy_auth=sogo_proxy_auth,
+        sogo_proxy_pass=sogo_proxy_pass,
+        sogo_eas_proxy_pass=sogo_eas_proxy_pass
+    )
+
+    with open(f"/etc/nginx/includes/{config_name}", "w") as f:
+        f.write(config)
+
+def nginx_conf(env):
+    config_name = "nginx.conf"
+    template = env.get_template(f"{config_name}.j2")
+
+    ssl_dir = '/etc/ssl/mail/'
+    valid_cert_dirs = []
+    for d in os.listdir(ssl_dir):
+        full_path = os.path.join(ssl_dir, d)
+        if not os.path.isdir(full_path):
+            continue
+
+        cert_path = os.path.join(full_path, 'cert.pem')
+        key_path = os.path.join(full_path, 'key.pem')
+        domains_path = os.path.join(full_path, 'domains')
+
+        if os.path.isfile(cert_path) and os.path.isfile(key_path) and os.path.isfile(domains_path):
+            with open(domains_path, 'r') as file:
+                domains = file.read().strip()
+            if domains and domains != os.getenv("MAILCOW_HOSTNAME", ""):
+                valid_cert_dirs.append({
+                    'cert_path': full_path + '/',
+                    'domains': domains.replace(',', ' ')
+                })
+
+    config = template.render(
+        cert_dirs=valid_cert_dirs,
+        MAILCOW_HOSTNAME=os.getenv("MAILCOW_HOSTNAME", ""),
+        ADDITIONAL_SERVER_NAMES=os.getenv("ADDITIONAL_SERVER_NAMES", "").replace(',', ' '),
+        HTTP_PORT=os.getenv("HTTP_PORT", "80"),
+        HTTPS_PORT=os.getenv("HTTPS_PORT", "443")
+    )
+
+    with open(f"/etc/nginx/{config_name}", "w") as f:
+        f.write(config)
+
+def wait_for_host(host, timeout=5):
+    while True:
+        try:
+            response = subprocess.run(["ping", "-c", "1", "-W", str(timeout), host], stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True)
+            
+            if response.returncode == 0:
+                break
+            else:
+                print(f"Waiting for {host}...")
+        except Exception as e:
+            print(f"An error occurred while trying to ping {host}: {e}")
+        
+        time.sleep(1)
+
+def main():
+    env = Environment(loader=FileSystemLoader('./etc/nginx/conf.d'))
+
+    # Generate config
+    print("Generate config")
+    sites_default_conf(env)
+    nginx_conf(env)
+    # Wait for services
+    wait_for_host("phpfpm")
+    wait_for_host("sogo")
+    wait_for_host("redis")
+    wait_for_host("rspamd")
+    # Validate config
+    print("Validate config")
+    subprocess.run(["nginx", "-qt"])
+
+    # Start NGINX
+    print("Starting NGINX")
+    subprocess.run(["nginx", "-g", "daemon off;"])
+
+
+if __name__ == "__main__":
+    main()
