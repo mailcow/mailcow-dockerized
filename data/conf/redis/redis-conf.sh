351f4ce787542b05dab8ad5c39c00429ef02233b
FreddleSpl0it <patschul@posteo.de>
2025-02-18 11:16:06 +0100

    [Redis] Add support for masterauth via env var

--- data/conf/redis/redis-conf.sh
+++ data/conf/redis/redis-conf.sh
@@ -5,4 +5,8 @@ requirepass $REDISPASS
 user quota_notify on nopass ~QW_* -@all +get +hget +ping
 EOF
 
+if [ -n "$REDISMASTERPASS" ]; then
+  echo "masterauth $REDISMASTERPASS" >> /redis.conf
+fi
+
 exec redis-server /redis.conf
