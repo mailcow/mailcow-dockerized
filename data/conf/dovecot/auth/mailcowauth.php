3014741aaaa1195fabce85a905b068fbbe58e6cd
FreddleSpl0it <patschul@posteo.de>
2025-04-15 10:49:56 +0200

    [Dovecot] Fix EAS login and improve logging

--- data/conf/dovecot/auth/mailcowauth.php
+++ data/conf/dovecot/auth/mailcowauth.php
@@ -80,7 +80,7 @@ if ($isSOGoRequest) {
 }
 if ($result === false){
   // If it's a SOGo Request, don't check for protocol access
-  $service = (isSOGoRequest) ? false : array($post['service'] => true);
+  $service = ($isSOGoRequest) ? false : array($post['service'] => true);
   $result = apppass_login($post['username'], $post['password'], $service, array(
     'is_internal' => true,
     'remote_addr' => $post['real_rip']
