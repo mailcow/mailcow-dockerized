d3185c3c68962a6fa6cf8d736ac87f99354080ce
FreddleSpl0it <patschul@posteo.de>
2025-05-16 13:37:49 +0200

    Add python bootstrapper for containers

---
+++ data/Dockerfiles/bootstrap/main.py
@@ -0,0 +1,30 @@
+import os
+import sys
+
+def main():
+  container_name = os.getenv("CONTAINER_NAME")
+
+  if container_name == "sogo-mailcow":
+    from modules.BootstrapSogo import Bootstrap
+  else:
+    print(f"No bootstrap handler for container: {container_name}", file=sys.stderr)
+    sys.exit(1)
+
+  b = Bootstrap(
+    container=container_name,
+    db_config = {
+      "host": "localhost",
+      "user": os.getenv("DBUSER"),
+      "password": os.getenv("DBPASS"),
+      "database": os.getenv("DBNAME"),
+      "unix_socket": "/var/run/mysqld/mysqld.sock",
+      'connection_timeout': 2
+    },
+    db_table="service_settings",
+    db_settings=['sogo']
+  )
+
+  b.bootstrap()
+
+if __name__ == "__main__":
+  main()
