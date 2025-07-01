cf9c0a269adc2a3f06bc0c40edf4a058ae087c7c
pi0n00r <131018595+pi0n00r@users.noreply.github.com>
2025-06-30 21:33:34 -0400

    Update create_cold_standby.sh
    export REMOTE_SSH_USER=

--- create_cold_standby.sh
+++ create_cold_standby.sh
@@ -3,5 +3,6 @@
 export REMOTE_SSH_KEY=/root/.ssh/id_rsa
 export REMOTE_SSH_PORT=22
 export REMOTE_SSH_HOST=my.remote.host
+export REMOTE_SSH_USER=non-root-user
 
 /opt/mailcow-dockerized/helper-scripts/_cold-standby.sh
