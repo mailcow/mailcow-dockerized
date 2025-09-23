e90541eff8d436f0d01a057326ff995c3261f434
FreddleSpl0it <75116288+FreddleSpl0it@users.noreply.github.com>
2025-09-23 10:07:33 +0200

    [Web] Revert - allow "*" as wildcard domain

--- data/web/inc/functions.inc.php
+++ data/web/inc/functions.inc.php
@@ -1116,13 +1116,9 @@ function is_valid_domain_name($domain_name, $options = array()) {
   $domain_name = idn_to_ascii($domain_name, 0, INTL_IDNA_VARIANT_UTS46);
 
   if (isset($options['allow_wildcard']) && $options['allow_wildcard'] == true) {
-    // Remove '*.' if wildcard domains are allowed
+    // Remove '*.' if wildcard subdomains are allowed
     if (strpos($domain_name, '*.') === 0) {
       $domain_name = substr($domain_name, 2);
-    }
-    // Allow '*' as wildcard domain
-    if ($domain_name === "*") {
-      return true;
     }
   }
 
