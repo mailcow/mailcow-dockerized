0a6bf79584468f06bb91e91d4db87c3c69d885e4
Thomas Mills <accounts+github@tomjmills.co.uk>
2025-10-08 11:34:27 +0100

    Show app password for last logins

--- data/web/js/site/user.js
+++ data/web/js/site/user.js
@@ -105,10 +105,9 @@ jQuery(function($){
               $(".last-sasl-login").append(`
                 <li class="list-group-item d-flex justify-content-between align-items-start">
                   <div class="ms-2 me-auto d-flex flex-column">
-                    <div class="fw-bold">` + real_rip + `</div>
-                    <small class="fst-italic mt-2">` + service + ` ` + local_datetime + `</small>
+                    <div class="fw-bold">` + real_rip + ip_location + `</div>
+                    <small class="fst-italic mt-2">` + service + ` ` + local_datetime + `</small>` + app_password + `
                   </div>
-                  <span>` + ip_location + `</span>
                 </li>
               `);
             })
