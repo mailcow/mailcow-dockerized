bb49e21583a1458bee8f51e7b73043c5cf6999b1
Lorenzo Moscati <lorenzo@moscati.page>
2026-03-19 13:26:58 +0100

    Restored the original protocol detection, updated SCIM URL composition
    Restored the original protocol detection that was modified during development and badly restored.
    Updated the SCIM base URL building to avoid hardcoded protocol.

--- data/web/inc/functions.inc.php
+++ data/web/inc/functions.inc.php
@@ -2448,11 +2448,7 @@ function getBaseURL($protocol = null) {
   }
 
   if (!isset($protocol)) {
-    if (isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] === 'on') {
-      $protocol = 'https';
-    } else {
-      $protocol = 'http';
-    }
+    $protocol = isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] === 'on' ? 'https' : 'http';
   }
   $base_url = $protocol . '://' . $host;
 
